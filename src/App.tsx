449197dacb2c4c49d261ced4993cd9a992b73004
Daniela Muller <danielamuller@Danielas-MacBook-Pro.local>
2023-08-14 09:39:30 -0300

    feat: add routes

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import { ThemeProvider } from 'styled-components'
+import { BrowserRouter } from 'react-router-dom'
+import { defaultTheme } from './styles/theme/default'
+import { GlobalStyles } from './styles/global'
+import { Router } from './Router'
+
+export function App() {
+  return (
+    <ThemeProvider theme={defaultTheme}>
+      <BrowserRouter>
+        <Router />
+      </BrowserRouter>
+      <GlobalStyles />
+    </ThemeProvider>
+  )
+}
